9318e733dab3e8d49b5926e5a04b90fe6d5d36ae
Shantanu Badmanji <shantanubadmanji1912@gmail.com>
2024-02-18 23:56:40 +0530

    upload the song file metadata to mongodb.  Changes to be committed: 	modified:   app/api/files/upload/route.js 	modified:   app/files/upload/page.jsx 	modified:   lib/database/models/File.js

--- app/api/files/upload/route.js
+++ app/api/files/upload/route.js
@@ -6,6 +6,9 @@ const { jwtVerify } = require("jose-node-cjs-runtime");
 const { createSecretKey } = require("crypto");
 
 import crypto from "crypto";
+import { File } from "@/lib/database/models/File";
+import { Schema } from "mongoose";
+import { connectToDatabase } from "@/lib/database/dbUtils";
 
 const s3 = new S3Client({
   region: process.env.AWS_BUCKET_REGION,
@@ -26,22 +29,19 @@ export async function POST(request) {
   const buffer = Buffer.from(await file.arrayBuffer());
   const hexFileName = crypto.randomBytes(32).toString("hex");
   //   secrete key
-  const secretKey = createSecretKey(process.env.JWT_STRING, "utf-8");
+  // const secretKey = createSecretKey(process.env.JWT_STRING, "utf-8");
 
-  let token = request.headers.get("cookie").split(" ")[1];
-  if (!token.startsWith("OutSiteJWT="));
-  //   console.log(token);
-  token = token.split("=")[1];
-  const {
-    payload: { id },
-    protectedHeader,
-  } = await jwtVerify(token, secretKey, {
-    issuer: "urn:example:issuer", // issuer
-    audience: "urn:example:audience", // audience
-  });
+  // let token = request.headers.get("cookie").split(" ")[1];
+  // if (!token?.startsWith("OutSiteJWT="));
+  // //   console.log(token);
+  // token = token?.split("=")[1];
+  // const { payload, protectedHeader } = await jwtVerify(token, secretKey, {
+  //   issuer: "urn:example:issuer", // issuer
+  //   audience: "urn:example:audience", // audience
+  // });
 
-  const userId = id;
-  console.log(userId);
+  // const userId = payload?.id || "";
+  // console.log(userId);
   const putObjCmd = new PutObjectCommand({
     Bucket: process.env.AWS_BUCKET_NAME,
     Key: hexFileName,
@@ -53,5 +53,18 @@ export async function POST(request) {
   const publicURL = (await getSignedUrl(s3, putObjCmd)).split("?")[0];
   console.log("publicURL: ", publicURL);
   // send the publicURL to metadata storage
+  await connectToDatabase();
+  
+  const dbFile = new File({
+    title: title,
+    author: "65d24d6e89549b38d9051b06",
+    genre: genre,
+    price: price,
+    url: publicURL,
+    imageUrl: "thumbnail-url",
+    rating: 5,
+    tags: tags,
+  });
+  await dbFile.save();
   return NextResponse.json({ success: "the file is uploaded successfully" });
 }
