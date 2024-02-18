411d20b9458d24f5ab44dc2e9ec960257babff9f
Adinath Shelke <shelkeadinath3@gmail.com>
2024-02-18 09:27:52 +0530

    working on navbar

--- components/shared/Navbar.jsx
+++ components/shared/Navbar.jsx
@@ -21,9 +21,9 @@ const Navbar = () => {
                   r="96"
                   fill="none"
                   stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="16"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="16"
                 ></circle>
                 <circle
                   cx="128"
@@ -31,17 +31,17 @@ const Navbar = () => {
                   r="40"
                   fill="none"
                   stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="16"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="16"
                 ></circle>
                 <path
                   d="M63.79905,199.37405a72.02812,72.02812,0,0,1,128.40177-.00026"
                   fill="none"
                   stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="16"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="16"
                 ></path>
               </svg>
               Account
@@ -49,7 +49,7 @@ const Navbar = () => {
           </a>
         </li>
         <li>
-          <a href="#" class="">
+          <a href="#" className="">
             <span>
               <svg
                 xmlns="http://www.w3.org/2000/svg"
@@ -63,25 +63,25 @@ const Navbar = () => {
                   d="M224,177.32122V78.67878a8,8,0,0,0-4.07791-6.9726l-88-49.5a8,8,0,0,0-7.84418,0l-88,49.5A8,8,0,0,0,32,78.67878v98.64244a8,8,0,0,0,4.07791,6.9726l88,49.5a8,8,0,0,0,7.84418,0l88-49.5A8,8,0,0,0,224,177.32122Z"
                   fill="none"
                   stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="16"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="16"
                 ></path>
                 <polyline
                   points="177.022 152.511 177.022 100.511 80 47"
                   fill="none"
                   stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="16"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="16"
                 ></polyline>
                 <polyline
                   points="222.897 74.627 128.949 128 33.108 74.617"
                   fill="none"
                   stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="16"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="16"
                 ></polyline>
                 <line
                   x1="128.94915"
@@ -90,9 +90,9 @@ const Navbar = () => {
                   y2="234.82131"
                   fill="none"
                   stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="16"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="16"
                 ></line>
               </svg>
               Shipping
@@ -116,10 +116,10 @@ const Navbar = () => {
                   width="208"
                   height="144"
                   rx="8"
-                  stroke-width="16"
+                  strokeWidth="16"
                   stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
                   fill="none"
                 ></rect>
                 <line
@@ -129,9 +129,9 @@ const Navbar = () => {
                   y2="168"
                   fill="none"
                   stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="16"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="16"
                 ></line>
                 <line
                   x1="119.99414"
@@ -140,9 +140,9 @@ const Navbar = () => {
                   y2="168"
                   fill="none"
                   stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="16"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="16"
                 ></line>
                 <line
                   x1="23.99414"
@@ -151,9 +151,9 @@ const Navbar = () => {
                   y2="96.85228"
                   fill="none"
                   stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="16"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="16"
                 ></line>
               </svg>
               Payment
@@ -178,9 +178,9 @@ const Navbar = () => {
                   y2="152"
                   fill="none"
                   stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="16"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="16"
                 ></line>
                 <line
                   x1="96"
@@ -189,25 +189,25 @@ const Navbar = () => {
                   y2="120"
                   fill="none"
                   stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="16"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="16"
                 ></line>
                 <path
                   d="M160.00194,40H200a8,8,0,0,1,8,8V216a8,8,0,0,1-8,8H56a8,8,0,0,1-8-8V48a8,8,0,0,1,8-8H95.99783"
                   fill="none"
                   stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="16"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="16"
                 ></path>
                 <path
                   d="M88,72V64a40,40,0,0,1,80,0v8Z"
                   fill="none"
                   stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="16"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="16"
                 ></path>
               </svg>
               Summary
