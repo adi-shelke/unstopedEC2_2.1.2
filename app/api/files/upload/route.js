a18cacd9246b3cf66045938eea726c10f3b12590
Shreyash Nalawade <shreyashnalawade11@gmail.com>
2024-02-18 18:07:43 +0530

    updated file upload route

--- app/api/files/upload/route.js
+++ app/api/files/upload/route.js
@@ -1,6 +1,10 @@
 import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 import { NextResponse } from "next/server";
+
+const { jwtVerify } = require("jose-node-cjs-runtime");
+const { createSecretKey } = require("crypto");
+
 import crypto from "crypto";
 
 const s3 = new S3Client({
@@ -15,7 +19,23 @@ export async function POST(request) {
   const file = (await request.formData()).get("file");
   const buffer = Buffer.from(await file.arrayBuffer());
   const hexFileName = crypto.randomBytes(32).toString("hex");
+  //   secrete key
+  const secretKey = createSecretKey(process.env.JWT_STRING, "utf-8");
 
+  let token = request.headers.get("cookie").split(" ")[1];
+  if (!token.startsWith("OutSiteJWT="));
+  //   console.log(token);
+  token = token.split("=")[1];
+  const {
+    payload: { id },
+    protectedHeader,
+  } = await jwtVerify(token, secretKey, {
+    issuer: "urn:example:issuer", // issuer
+    audience: "urn:example:audience", // audience
+  });
+
+  const userId = id;
+  console.log(userId);
   const putObjCmd = new PutObjectCommand({
     Bucket: process.env.AWS_BUCKET_NAME,
     Key: hexFileName,
