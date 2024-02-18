915d0cc50c4e5865541c7e99908c92145c68f0ba
Adinath Shelke <shelkeadinath3@gmail.com>
2024-02-18 14:38:49 +0530

    changed file upload form

--- app/files/layout.jsx
+++ app/files/layout.jsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const layout = ({ children }) => {
   return (
-    <div className="w-[100vw] bg-[#363849]">
+    <div className="w-[100vw] min-h-screen bg-[#363849]">
       <Navbar />
       {children}
     </div>
--- app/files/upload/page.jsx
+++ app/files/upload/page.jsx
@@ -1,70 +1,156 @@
 "use client";
-import { useState } from "react";
+import Link from "next/link";
+import React, { useState } from "react";
 
-const UploadPage = () => {
-  const [file, setFile] = useState(null);
-  const [error, setError] = useState(null);
+const SignUp = () => {
+  const [formData, setFormData] = useState({
+    title: "",
+    price: "",
+    tags: "",
+    file: "",
+    genre: "",
+  });
+
+  const [errors, setErrors] = useState({});
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
 
   const handleFileChange = (e) => {
     const selectedFile = e.target.files[0];
     const fileSizeLimit = 8 * 1024 * 1024; // 8 MB
-
     if (selectedFile && selectedFile.size <= fileSizeLimit) {
-      setFile(selectedFile);
-      setError(null);
+      setFormData({
+        ...formData,
+        file: selectedFile,
+      });
+      setErrors({});
     } else {
-      setFile(null);
-      setError("Please select a file that is smaller than 8 MB.");
+      setFormData({
+        ...formData,
+        file: null,
+      });
+      setErrors({ file: "Please select a file that is smaller than 8 MB." });
     }
   };
-
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    if (file) {
-      // TODO: Submit the file to the backend route
-      console.log("File submitted:", file);
-      const form = new FormData();
-      form.append("file", file);
-      const response = await fetch("/api/files/upload", {
+    // Basic validation
+    const newErrors = {};
+    if (!formData.title || formData.length < 3) {
+      newErrors.title = "Please enter valid title of track";
+    }
+    if (!formData.price || formData.price < 0) {
+      newErrors.price = "Please enter valid price of track";
+    }
+    if (!formData.file) {
+      newErrors.file = "Please select a file to upload";
+    }
+
+    setErrors(newErrors);
+
+    // Submit form if no errors
+    if (Object.keys(newErrors).length === 0) {
+      const options = {
         method: "POST",
-        body: form,
-      }).then((res) => res.json());
-      console.log("response: ", response);
-    } else {
-      setError("Please select a file to upload.");
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      };
+      const response = await fetch("/api/files/upload", options);
+      // Here you can submit the form data
+      console.log("Form submitted:", formData);
     }
   };
 
   return (
-    <div className="container p-4 flex justify-center items-center w-[100vw] bg-[#363849]  h-[100vh]">
-      <div className="bg-[#3a5ba0] text-white p-4 rounded-lg w-[40%] h-[40%]">
-        <h1 className="text-2xl font-bold mb-4">Upload Audio File</h1>
-        <form onSubmit={handleSubmit}>
-          <div className="mb-4">
-            <label htmlFor="fileInput" className="block mb-2 font-medium">
-              Select an audio file (max 8 MB):
-            </label>
-            <input
-              type="file"
-              id="fileInput"
-              accept="audio/*"
-              onChange={handleFileChange}
-              className=" w-full px-3 py-2  hover:shadow-lg duration-300 ease-in-out hover:shadow-black border border-gray-700 bg-[#ffffff] rounded-md placeholder-gray-500 text-black focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-            />
-            {file && <span className="text-gray-500">{file.name}</span>}
-            {error && <p className="text-red-500 mt-2">{error}</p>}
-          </div>
-          <button
-            type="submit"
-            className="bg-gray-800 text-white rounded-md hover:bg-[#FF8911] px-3 py-2"
-          >
-            Upload
-          </button>
-        </form>
-      </div>
+    <div className="flex items-center min-h-screen">
+      <form className=" mx-auto p-6 rounded-lg shadow-md bg-[#3a5ba0] w-[60%] sm:w-[30%]">
+        <h2 className="text-2xl font-bold mb-4 text-white">Sign up</h2>
+        <div className="mb-4">
+          <input
+            type="text"
+            name="title"
+            value={formData.title}
+            onChange={handleChange}
+            placeholder="Enter title of track"
+            className={`w-full px-3 py-2 hover:shadow-lg duration-500 ease-in-out hover:shadow-black border border-gray-700 bg-white rounded-md placeholder-gray-500 text-black focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm ${
+              errors?.title ? "border-red-500" : ""
+            }`}
+          />
+          {errors?.title && (
+            <p className="text-red-500 text-sm mt-1">{errors?.title}</p>
+          )}
+        </div>
+        <div className="mb-4">
+          <input
+            type="number"
+            name="price"
+            value={formData.price}
+            onChange={handleChange}
+            placeholder="Enter price of track"
+            className={`w-full px-3 py-2  hover:shadow-lg duration-500 ease-in-out hover:shadow-black border border-gray-700 bg-white rounded-md placeholder-gray-500 text-black focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm${
+              errors?.price ? "border-red-500" : ""
+            }`}
+          />
+          {errors?.price && (
+            <p className="text-red-500 text-sm mt-1">{errors?.price}</p>
+          )}
+        </div>
+        <div className="mb-4">
+          <input
+            type="email"
+            name="tags"
+            value={formData.tags}
+            onChange={handleChange}
+            placeholder="Enter tags"
+            className={`w-full px-3 py-2   hover:shadow-lg duration-500 ease-in-out hover:shadow-black border border-gray-700 bg-[#ffffff] rounded-md placeholder-gray-500 text-black focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm ${
+              errors?.tags ? "border-red-500 " : ""
+            }`}
+          />
+        </div>
+        <div className="mb-4">
+          <input
+            type="text"
+            name="genre"
+            value={formData.genre}
+            onChange={handleChange}
+            placeholder="Enter genre of track"
+            className={`w-full px-3 py-2   hover:shadow-lg duration-500 ease-in-out hover:shadow-black border border-gray-700 bg-[#ffffff] rounded-md placeholder-gray-500 text-black focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm ${
+              errors?.genre ? "border-red-500 " : ""
+            }`}
+          />
+        </div>
+        <div className="mb-4">
+          <input
+            type="file"
+            name="tags"
+            onChange={handleFileChange}
+            placeholder="Enter tags"
+            className={`w-full px-3 py-2   hover:shadow-lg duration-500 ease-in-out hover:shadow-black border border-gray-700 bg-[#ffffff] rounded-md placeholder-gray-500 text-black focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm ${
+              errors?.file ? "border-red-500 " : ""
+            }`}
+          />
+          {errors?.file && (
+            <p className="text-red-500 text-sm mt-1">{errors?.file}</p>
+          )}
+        </div>
+        <button
+          type="submit"
+          onClick={handleSubmit}
+          className="w-full py-2 bg-gray-800 text-white rounded-md hover:bg-[#FF8911] focus:outline-none"
+        >
+          Upload File
+        </button>
+      </form>
     </div>
   );
 };
 
-export default UploadPage;
+export default SignUp;
