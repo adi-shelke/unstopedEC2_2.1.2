9b99117c332875777b348918b4505ac06e417ef0
Adinath Shelke <shelkeadinath3@gmail.com>
2024-02-18 10:49:34 +0530

    working on landing page

--- app/auth/layout.jsx
+++ app/auth/layout.jsx
@@ -1,9 +1,10 @@
-
+import Navbar from "@/components/shared/Navbar";
 import React from "react";
 
 const layout = ({ children }) => {
   return (
     <div className="bg-[#363849]">
+      <Navbar />
       {children}
     </div>
   );
--- components/shared/Navbar.jsx
+++ components/shared/Navbar.jsx
@@ -1,11 +1,14 @@
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 const Navbar = () => {
   return (
     <div className="h-[60px] flex items-center w-full justify-between bg-[#363849] mt-4 px-7">
       <div className="text-white">
-        <h1 className="">BeatMakers</h1>
+        <Link href="/">
+          <p>BeatMakers</p>
+        </Link>
       </div>
       <div className="h-[70%] rounded-full flex w-[60%]  justify-center">
         <input className=" h-full rounded-full w-[70%] mx-2 px-2" />
@@ -18,7 +21,7 @@ const Navbar = () => {
       </div>
       <ul className="">
         <li>
-          <a href="#">
+          <Link href="/profile">
             <span className="">
               <Image
                 src="/assets/icons/profile.png"
@@ -28,10 +31,10 @@ const Navbar = () => {
               />
               <p>Profile</p>
             </span>
-          </a>
+          </Link>
         </li>
         <li>
-          <a href="#" className="w-full">
+          <Link href="/">
             <span>
               <Image
                 src="/assets/icons/tracks.png"
@@ -41,15 +44,15 @@ const Navbar = () => {
               />
               Tracks
             </span>
-          </a>
+          </Link>
         </li>
         <li>
-          <a href="#">
+          <Link href="/auth/login">
             <span>
               <Image src="/assets/icons/login.png" width={20} height={20} />
               Login
             </span>
-          </a>
+          </Link>
         </li>
         {/* <li>
           <a href="#">
